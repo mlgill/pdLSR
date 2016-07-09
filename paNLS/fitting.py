d8d36cd0a34127fbe81aadc391da79e507ced337
Michelle Gill <github@michellelynngill.com>
2016-07-09 19:06:22 -0400

    Added old version of ci function

--- paNLS/fitting.py
+++ paNLS/fitting.py
@@ -143,6 +143,52 @@ def get_confidence_interval(fit_data, sigma, threads=None):
     return ciobj.tolist()
 
 
+# def get_confidence_interval(fitobj_df, mask, sigma, threads=None):
+
+#     if mask.sum() == 0:
+#         print('There is not enough data to calculate a confidence interval.')
+#         # TODO exit here
+
+#     # For multithreaded implementation
+#     def _apply_df(args):
+#         df, func, kwargs = args
+#         return df.apply(eval(func), axis=1, **kwargs)
+
+#     def apply_by_multiprocessing(df, func, **kwargs):
+#         threads = kwargs.pop('threads')
+#         pool = multiprocessing.Pool(processes=threads)
+#         result = pool.map(_apply_df, [(d, func, kwargs)
+#                                       for d in np.array_split(df, threads)])
+#         pool.close()
+#         return pd.concat(list(result))
+
+#     # Make this a string and use eval in _apply_df to get around a pickling issue with lmfit.conf_interval/lambda
+#     # Right know this module also prefers multiprocessing with dill, which may still be more robust
+#     ci_func = 'lambda x: lmfit.conf_interval(x.minimizer, x.fitobj, sigmas=[{:.2f}])'.format(sigma)
+
+#     if threads is None:
+#         threads = multiprocessing.cpu_count()
+
+#     # This is a bit of a hack and is required to get this to work with dataframes
+#     fitobj_noidx = fitobj_df.loc[mask, ['minimizer','fitobj']].reset_index(drop=True)
+
+#     # Try to use parallelized version first, switch to unparallelized if error occurs 
+#     try:
+#         ciobj = apply_by_multiprocessing(fitobj_noidx, 
+#                                          ci_func, 
+#                                          threads=threads).dropna(axis=1).squeeze()
+#     except:
+#         print('Unable to use multithreading for confidence interval calculation. This might take a while.')
+#         ciobj = fitobj_noidx.apply(lambda x: lmfit.conf_interval(x.minimizer, 
+#                                                                  x.fitobj, 
+#                                                                  sigmas=[sigma]), 
+#                                           axis=1)
+
+#     ciobj.set_axis(0, fitobj_df.loc[mask].index)
+#     ciobj.name = 'ciobj'
+
+#     return ciobj
+
 
 # Function to predict y-values based on fit parameters and an x-array
 def predict(fit_data, groupcols, xtype='global', xnum=50, xcalc=None):
