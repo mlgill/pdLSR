83c0775b5c3f2f24f50fab0a7a65d3148f734abe
Michelle Gill <github@michellelynngill.com>
2016-07-09 21:22:11 -0400

    Update todos

--- pdlmfit/__init__.py
+++ pdlmfit/__init__.py
@@ -3,10 +3,14 @@ from .pdlmfit import pdlmfit
 
 # TODOs:
 # [ ] add docstrings
+# [ ] make the predict function work
+# [ ] add common functions
+# [ ] try with multiple variables?
+
 # [ ] check that groupcols are in the data, otherwise quit
 # [ ] check that xname/yname/yerr are in the data, otherwise quit
 # [ ] check that method is leastsq, otherwise quit
 # [ ] search for NaNs in model column and fill
 # [ ] make unrecognized parameter format throw an error and quit
-# [ ] make the predict function work
+
 # [ ] compatibility with Python 3?
